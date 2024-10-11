2f577cee794d625014a60cc77f9d14f0dd45171f
LT-Nhat-Minh <22520863@gm.uit.edu.vn>
2024-10-11 07:14:01 +0700

    update #9.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,51 @@
 import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from "./redux/counter/counterSlice";
-import styles from "./styles/Counter.module.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/login";
+import Contact from "./pages/contact";
+import Book from "./pages/book";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+
+const Layout = () => {
+  return (
+    <div className="layout-app">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 export default function App() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState("2");
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <div>404 Not Found</div>,
 
-  const incrementValue = Number(incrementAmount) || 0;
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
+        {
+          path: "book",
+          element: <Book />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
---
+++ src/pages/book/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Book() {
+  return <div>book page</div>;
+}
+
+export default Book;
